8a256c8bab3a4ac1139fbb80a1801a2f8f3ad263
Wilmer Gomez Prado <wgomezpr@emeal.nttdata.com>
2024-07-22 17:59:39 -0500

    UPDATE

--- Signature.WebAPI/Entities/Requests/FailedTicketRequest.cs
+++ Signature.WebAPI/Entities/Requests/FailedTicketRequest.cs
@@ -2,19 +2,26 @@
 {
     public class FailedTicketRequest
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        #region Propiedades
         /// <summary>
         /// CodigoEmpresa
         /// </summary>
-        public string Empresa { get; set; }
+        public string CodigoEmpresa { get; set; }
+
         /// <summary>
         /// FechaIni
         /// </summary>
         public string FechaIni { get; set; }
+
         /// <summary>
         /// FechaFin
         /// </summary>
         public string FechaFin { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        /// <summary>
+        /// Operador
+        /// </summary>
+        public string Operador { get; set; }
+        #endregion
     }
 }
